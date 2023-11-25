8329512ad1b2c3a70c613d9ac36ba8b74fa7a435
redmac135 <ethanzhao1981@gmail.com>
2023-11-25 11:44:28 -0500

    create unmigrated backend structure

---
+++ backend/vocalreflections/entries/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from reflectors.models import Reflector
+
+
+# Create your models here.
+class Entry(models.Model):
+    reflector = models.ForeignKey(Reflector, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    audio = models.FileField(upload_to="audio/")
+
+    def __str__(self):
+        return self.title
