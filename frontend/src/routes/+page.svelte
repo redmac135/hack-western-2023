eedb2e0eda91cd302efe6900165278abc66fc6d4
redmac135 <ethanzhao1981@gmail.com>
2023-11-25 10:36:03 -0500

    add basic tutorial

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	// script goes at the top in here
+	// dont' worry too much about this, we just need to get a basic good looking frontend first
+</script>
+
+<!-- you can write any html here -->
+<h1>This is my title!</h1>
+<p>here's some body tages</p>
+
+<style>
+	/* then your styles go at the bottom in a "style" tag */
+
+	h1 {
+		color: red;
+	}
+
+	p {
+		color: blue;
+	}
+</style>
