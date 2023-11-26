c3f770133c20cdcfbb090ca1d530661471297dbb
redmac135 <ethanzhao1981@gmail.com>
2023-11-26 02:52:13 -0500

    navbar styling

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,3 +4,25 @@
 	<a href="/summary">Contact</a>
 </nav>
 <slot />
+
+<style>
+	nav {
+		display: flex;
+		justify-content: right;
+		align-items: center;
+		width: 100%;
+		height: 4rem;
+	}
+
+	a {
+		padding: 0.1rem;
+		margin: 0.4rem;
+		color: rgb(70, 70, 70);
+		text-decoration: none;
+		transition: border-bottom 0.3s;
+	}
+
+	a:hover {
+		border-bottom: 1px solid black;
+	}
+</style>
